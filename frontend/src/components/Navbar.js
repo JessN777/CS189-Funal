23018b711f856ac167bf13170073e122aa1116f1
Noris-0 <sanyuemyname@gmail.com>
2024-12-05 15:05:42 -0800

    testing on bootstrap design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../style/style.scss';
 
 function Navbar() {
     return (
-        <nav>
+        <nav className='navbar navbar-expand-lg bg-primary text-success'>
             <ul>
                 <li><Link to="/">Search</Link></li>
                 <li><Link to="/daily-recommendations">Daily Recommendations</Link></li>
